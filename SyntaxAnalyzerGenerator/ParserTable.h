5e59cd300f330338c139c6728b97e58c3a6ebc27
M2 <mhmdalaa333@gmail.com>
2020-04-16 06:34:52 +0200

    implementation of the parserTable

--- SyntaxAnalyzerGenerator/ParserTable.h
+++ SyntaxAnalyzerGenerator/ParserTable.h
@@ -1,33 +1,54 @@
-//
-// Created by OWNER on 4/13/2020.
-//
-
 #ifndef COMPILER_PARSERTABLE_H
 #define COMPILER_PARSERTABLE_H
 
 #include "SyntaxAnalyzerUtilities.h"
-enum EntryType{
-    production,sync,error
+
+enum EntryType {
+    production, sync, error
 };
+
 class ParserTable {
 private:
-    ContextFreeGrammar contextFreeGrammar;
-    unordered_map<Symbol,unordered_map<Symbol,pair<int,int>,SymbolHF>,SymbolHF> production_entries;
-    unordered_map<Symbol,unordered_set<Symbol,SymbolHF>,SymbolHF> epsilon_entries;
-    unordered_map<Symbol,unordered_set<Symbol,SymbolHF>,SymbolHF> sync_entries;
+    ContextFreeGrammar cfg;
+    unordered_map<Symbol, unordered_map<Symbol, pair<int, int>, SymbolHF>, SymbolHF> production_entries;
+    unordered_map<Symbol, unordered_set<Symbol, SymbolHF>, SymbolHF> sync_entries;
+
+    void validate_input(const Symbol &non_terminal, const Symbol &terminal);
+
+    unordered_map<Symbol, pair<int, int>, SymbolHF> build_production_row(int, SyntaxAnalyzerUtilities);
+
+    unordered_set<Symbol, SymbolHF> build_sync_row(int, SyntaxAnalyzerUtilities);
+
+    vector<vector<string>> table_to_string();
+
+    vector<int> get_entries_widths(const vector<vector<string>> &);
+
+    string symbol_vector_to_string(const vector<Symbol> &list);
 
 public:
-    //TODO check ambiguous grammar
-    ParserTable(const ContextFreeGrammar&);
-    EntryType get_entry_type(Symbol non_terminal,Symbol terminal);
+
+    ParserTable(const ContextFreeGrammar &);
+
     /**
-     *
-     * @param non_terminal
-     * @param terminal
-     * @return vector<Symbol> contains right hand side of entry production
-     * @throw exception if the entry contains no production
+     * This function is used to check the type of the entry before using get_entry function.
+     * @param non_terminal The Symbol which determines the row in the table.
+     * @param terminal The Symbol which determines the column in the table.
+     * @return Return the type of the entry in the table i.e.(production, sync, error).
+     * @throw Throw exception if the input is invalid.
      */
-    vector<Symbol> get_entry(Symbol non_terminal,Symbol terminal);
+    EntryType get_entry_type(const Symbol &non_terminal, const Symbol &terminal);
+
+    /**
+     * This function is used to locate a production in the table.
+     * @warning This function is only used if the type of the entry is production (check entry type using get_entry_type function).
+     * @param non_terminal The Symbol which determines the row in the table.
+     * @param terminal The Symbol which determines the column in the table.
+     * @return vector<Symbol> contains right hand side of entry production.
+     * @throw Throw exception if the entry contains no production or invalid input.
+     */
+    vector<Symbol> get_entry(const Symbol &non_terminal, const Symbol &terminal);
+
+    void print();
 
 };
 
